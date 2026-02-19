4430627e5dc3e6a43b1dda8e01090a13b0261e76
Saraniga1 <159415524+Saraniga1@users.noreply.github.com>
2026-02-19 12:39:17 +0530

    day3 – Services & Dependency Injection

--- FirstAPI/Controllers/ProductController.cs
+++ FirstAPI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using FirstAPI.Models;
+using FirstAPI.Services;
 
 namespace FirstAPI.Controllers
 {
@@ -7,18 +8,20 @@ namespace FirstAPI.Controllers
     [Route("[controller]")]
     public class ProductController : ControllerBase
     {
-        private static List<Product> products = new List<Product>();
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
 
         [HttpGet]
-        public IActionResult GetAll()
-        {
-            return Ok(products);
-        }
+        public IActionResult GetAll() => Ok(_service.GetAll());
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            var product = products.FirstOrDefault(p => p.Id == id);
+            var product = _service.GetById(id);
             if (product == null)
                 return NotFound("Product Not Found");
 
@@ -33,40 +36,35 @@ namespace FirstAPI.Controllers
             if (product.Price <= 0)
                 return BadRequest("Price must be greater than zero");
 
-            product.Id = products.Count == 0 ? 1 : products.Max(p => p.Id) + 1;
+            var createdProduct = _service.Create(product);
 
-            products.Add(product);
-
-            return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+            return CreatedAtAction(nameof(GetById), new { id = createdProduct.Id }, createdProduct);
         }
 
         [HttpPut("{id}")]
         public IActionResult Update(int id, Product updatedProduct)
         {
-            var product = products.FirstOrDefault(p => p.Id == id);
 
-            if (product == null)
-                return BadRequest("Product Not Found");
             if (string.IsNullOrWhiteSpace(updatedProduct.Name))
                 return BadRequest("Product name is required");
             if (updatedProduct.Price <= 0)
                 return BadRequest("Price must be geater than zero");
 
-            product.Name = updatedProduct.Name;
-            product.Price = updatedProduct.Price;
+            var sucess = _service.Update(id, updatedProduct);
 
-            return Ok(product);
+            if (!sucess)
+                return NotFound("Product Not Found");
+
+            return Ok(updatedProduct);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var product = products.FirstOrDefault(p => id == p.Id);
+            var sucess = _service.Delete(id);
 
-            if (product == null)
+            if (!sucess)
                 return NotFound("Product not found");
-
-            products.Remove(product);
 
             return NoContent();
         }
